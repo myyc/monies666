2f4f9f32ce0cd618926c486e5144cf71cf7a9c7a
myyc <asd@asd.asd>
2014-02-25 00:32:30 +0100

    depends on mongodb now

--- stocks.py
+++ stocks.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, json
 import requests
-import time, datetime
+import time
+import datetime
+import pymongo
 
 app = Flask(__name__)
 
@@ -15,50 +17,96 @@ cache_opts = {
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
 
+getts = lambda d: int(time.mktime(datetime.datetime.strptime(d, "%Y-%m-%d").timetuple()))
 
-@app.route("/")
-@app.route("/hi/")
-@app.route("/hi/<symbols>")
-def hello(symbols=None):
+
+def reweigh(l, n):
+    l["values"] = [{"x": d["x"], "y": d["y"] / (0.0001 + n * l["values"][0]["y"])} for d in l["values"]]
+    return l
+
+
+@app.route("/hi/<symbols>", defaults={"fromdate": None, "todate": None})
+@app.route("/hi/<symbols>/<todate>", defaults={"fromdate": None})
+@app.route("/hi/<symbols>/<fromdate>/<todate>")
+def hello(symbols=None, fromdate=None, todate=None):
     if symbols is None:
         symbols = "AAPL"
-    return render_template("main.html", sym=symbols)
+    args = symbols
+    if fromdate is not None:
+        args += "/" + fromdate
+    if todate is not None:
+        args += "/" + todate
+    return render_template("main.html", args=args)
 
 
-@app.route("/fetch/")
-@app.route("/fetch/<symbols>")
-def fetch(symbols=None):
+@app.route("/fetch/<symbols>", defaults={"fromdate": None, "todate": None})
+@app.route("/fetch/<symbols>/<fromdate>", defaults={"todate": None})
+@app.route("/fetch/<symbols>/<fromdate>/<todate>")
+def fetch(symbols, fromdate=None, todate=None):
+    todate = datetime.datetime.today() if todate is None else datetime.datetime.strptime(todate, "%Y-%m-%d")
+    fromdate = datetime.datetime.today() - datetime.timedelta(
+        days=360) if fromdate is None else datetime.datetime.strptime(fromdate, "%Y-%m-%d")
     symbols = symbols.split(",")
-    return json.dumps([get(sym, len(symbols)) for sym in symbols])
+    return json.dumps([reweigh(get(sym, fromdate, todate), len(symbols)) for sym in symbols])
 
 
-#@cache.cache("getsym", expire=600)
-def get(symbol, n):
-    url = "http://ichart.finance.yahoo.com/table.csv"
-    p = {
-        "s": symbol,
-        "e": 18,
-        "d": 2,
-        "f": 2014,
-        "b": 18,
-        "a": 2,
-        "c": 2013
-    }
+def get(symbol, fromdate, todate):
+    conn = pymongo.Connection("127.0.0.1")
+    coll = conn["stocks"]["daily"]
+    cur = coll.find({"symbol": symbol, "date": {"$gte": fromdate, "$lte": todate}}).sort([("date", 1)])
+    if cur.count() == 0:
+        return getfromyahoo(symbol, fromdate, todate)
+    else:
+        return {"key": symbol, "values": [{"x": time.mktime(d["date"].timetuple()), "y": d["close"]} for d in cur]}
+
+
+def insertintomongo(symbol, text):
+    conn = pymongo.Connection("127.0.0.1")
+    coll = conn["stocks"]["daily"]
 
     def dec(r):
         r = r.split(",")
         return {
-            "x": int(time.mktime(datetime.datetime.strptime(r[0], "%Y-%m-%d").timetuple())),
+            "symbol": symbol,
+            "date": datetime.datetime.strptime(r[0], "%Y-%m-%d"),
+            "open": float(r[1]),
+            "high": float(r[2]),
+            "close": float(r[3]),
+            "volume": float(r[4]),
+            "adjclose": float(r[5])
+        }
+
+    o = [dec(r) for r in text.strip().split("\n")[1:]]
+    coll.insert(o)
+
+
+def getfromyahoo(symbol, fromdate, todate):
+    url = "http://ichart.finance.yahoo.com/table.csv"
+    p = {
+        "s": symbol,
+        "f": todate.year,
+        "d": todate.month - 1,
+        "e": todate.day,
+        "c": fromdate.year,
+        "a": fromdate.month - 1,
+        "b": fromdate.day,
+        "g": "d",
+        "ignore": ".csv"
+    }
+    text = requests.get(url, params=p).text
+
+    insertintomongo(symbol, text)
+
+    def dec(r):
+        r = r.split(",")
+        return {
+            "x": getts(r[0]),
             "y": float(r[4])
         }
 
-    data = [dec(r) for r in requests.get(url, params=p).text.split("\n")]
-    m = list(map(lambda d: {"x": d["x"], "y": d["y"] / (n * data[-1]["y"])}, data))
-    m.reverse()
-    print(m)
-    return {"key": symbol, "values": m}
+    return {"key": symbol, "values": [dec(r) for r in text.strip().split("\n")[1:]]}
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
